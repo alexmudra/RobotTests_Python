ec97d8ef9716d92af54910f354dd2794e9719427
mudra <alexander.kobko@gmail.com>
2020-09-09 20:19:28 +0300

    added sql file to project

---
+++ Exp_in_SDET_QA_Autom_Tech/TestData/myDB_with_persons_insertData.sql
@@ -0,0 +1,5 @@
+INSERT INTO mydb.person values(101,"Alex","K");
+INSERT INTO mydb.person values(102,"Ann", "M");
+INSERT INTO mydb.person values(103,"Kate","M")
+INSERT INTO mydb.person values(104,"Honey","K")
+INSERT INTO mydb.person values(105,"Vira","K")
