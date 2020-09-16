43961722a4f5d356dfd0ba62c84f5fdc752cdddd
mudra <alexander.kobko@gmail.com>
2020-09-16 20:20:25 +0300

    added Part 23- Page Object Model (POM) Pattern in Robot Framework | Selenium with Python

---
+++ Exp_in_SDET_QA_Autom_Tech/PageObjects/locators.py
@@ -0,0 +1,8 @@
+#https://www.youtube.com/watch?v=DCgrW-i9cT8&list=PLUDwpEzHYYLsCHiiihnwl3L0xPspL7BPG&index=23
+#Part 23- Page Object Model (POM) Pattern in Robot Framework | Selenium with Python
+
+
+#Login Page Elements
+txt_login_userName="xpath://input[@id='loginform-username']"
+txt_login_password="xpath://input[@id='loginform-username']"
+btn_signIn="xpath://button[@name='login-button']"
---
+++ Exp_in_SDET_QA_Autom_Tech/Resources/loginKeywords.robot
@@ -0,0 +1,4 @@
+#https://www.youtube.com/watch?v=DCgrW-i9cT8&list=PLUDwpEzHYYLsCHiiihnwl3L0xPspL7BPG&index=23
+#Part 23- Page Object Model (POM) Pattern in Robot Framework | Selenium with Python
+*** Settings ***
+Library  SeleniumLibrary
